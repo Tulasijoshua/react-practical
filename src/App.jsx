5572de1a1df4b00be80ac8af8210f0be75098070
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-12 21:59:50 +0000

    Feat: lazy loading component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import InputControlForm from './components/InputControlForm'
+import LazyComponent from './components/LazyComponent'
 
 function App() {
 
   return (
     <>
       <div>
-        <InputControlForm />
+        {/* <InputControlForm /> */}
+        <LazyComponent />
       </div>
     </>
   )
